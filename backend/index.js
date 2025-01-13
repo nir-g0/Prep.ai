810e089862c2cb0a91aae0c568e8df2666b3466e
nir-g0 <ngub24@gmail.com>
2025-01-12 16:39:35 -0800

    Simple server querying

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express') // Import express
+const app = express() // Create an express app
+const port = 3000 // Define the port number
 
+let user = { length: 0, users: {} }
+// Handle GET requests to "/"
 app.get('/', (req, res) => {
-  res.send('This is the backend')
+  const { name } = req.query // Extract "name" from the query parameters
+  if (name) {
+    user['length'] += 1
+    user['users'][name] = user['length']
+  }
+  res.json(user) // Respond with the name
 })
 
-//nodemon index.js
+// Start the server
 app.listen(port, () => {
   console.log(`Now listening on port ${port}`)
 })
