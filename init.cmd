526d7d3d5e805b4d83523e45468b015fda12404d
nir-g0 <ngub24@gmail.com>
2025-01-14 23:35:12 -0800

    added initialization command

---
+++ init.cmd
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd backend
+npm install
+cd src
+touch .env
+cd ../../
+# Navigate to the ml-service directory
+cd ml-service
+conda create -p venv python==3.12.7 -y
+source "$(conda info --base)/etc/profile.d/conda.sh"
+conda activate ./venv
+pip install -r requirements.txt
+python -m spacy download en_core_web_sm
+cd ..
+./run.cmd
